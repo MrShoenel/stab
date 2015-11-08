07305c0e0d11b6580677f76acb57bdd60400e31e
MrShoenel <development@hoenel.net>
2015-11-08 18:09:15 +0100

    Updated type-definitions

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,5 @@
 
 /// <reference path="angularjs/angular.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="oclazyload/oclazyload.d.ts" />
+/// <reference path="requirejs/require.d.ts" />
