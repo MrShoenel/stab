506ef51f940907701b4666700c9a3177f1912273
MrShoenel <development@hoenel.net>
2015-11-25 21:51:05 +0100

    Updated the Fragment-directive to always bind to the $rootScope

--- resource/app/fragment/fragment.directive.ts
+++ resource/app/fragment/fragment.directive.ts
@@ -12,8 +12,8 @@ module Blog {
 			restrict: 'E',
 			
 			template: '<div>' + (Config.get<boolean>('ALLOW_ANGULAR_HTML', false) ?
-				'<div bind-html-compile="vm.trustedValue"></div>' :
-				'<div ng-bind-html="vm.trustedValue"></div>') + '</div>',
+				'<div bind-html-compile="vm.trustedValue" bind-html-scope="$root"></div>' :
+				'<div ng-bind-html="vm.trustedValue" bind-html-scope="$root"></div>') + '</div>',
 			
 			controllerAs: 'vm',
 			controller: 'FragmentController',
