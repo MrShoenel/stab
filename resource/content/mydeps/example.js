72c78cd42c8356ae00e2545220e33994e9965080
Sebastian Hönel <sebastian.honel@softwerk.se>
2015-12-07 01:27:28 +0100

    Add two examples for what JS/CSS 'mydeps'

---
+++ resource/content/mydeps/example.js
@@ -0,0 +1,6 @@
+/**
+ * Here you can put content that will get loaded/resolved as
+ * part of the main state. Thus it will become available to
+ * the whole blog-application.
+ */
+console.log('A JavaScript dependency');
